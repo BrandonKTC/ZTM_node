2b9dcd11369b8c0c36da032f96e9803089c179dd
brandon <brandonkwamou@gmail.com>
2022-09-17 12:13:13 +0200

    planets

--- planets-project/index.js
+++ planets-project/index.js
@@ -1,7 +1,16 @@
 const { parse } = require('csv-parse')
 const fs = require('fs')
 
-const results = []
+const habitablePlanets = []
+
+const isHabitablePlanet = (planet) => {
+	return (
+		planet['koi_disposition'] === 'CONFIRMED' &&
+		planet['koi_insol'] > 0.36 &&
+		planet['koi_insol'] < 1.11 &&
+		planet['koi_prad'] < 1.6
+	)
+}
 
 fs.createReadStream('kepler_data.csv')
 	.pipe(
@@ -11,12 +20,12 @@ fs.createReadStream('kepler_data.csv')
 		})
 	)
 	.on('data', (data) => {
-		results.push(data)
+		if (isHabitablePlanet(data)) habitablePlanets.push(data)
 	})
 	.on('error', (err) => {
 		console.error(err)
 	})
 	.on('end', () => {
-		console.log(results)
-		console.log('done')
+		console.log(habitablePlanets.map((planet) => planets['kepler_name']))
+		console.log(`${habitablePlanets.length} habitable planets found`)
 	})
