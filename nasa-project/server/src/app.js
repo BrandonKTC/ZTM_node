61a4f01eac00217a5eecd1ed43fcee795ba41647
brandon kwamou <b.kwamoutchamou@gmail.com>
2022-10-30 14:31:37 +0100

    package json error

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
 
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
 
 module.exports = app;
