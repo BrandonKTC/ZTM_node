d70cb94c1875a34af3924f9759c4e59966a489ab
brandon kwamou <b.kwamoutchamou@gmail.com>
2022-10-30 12:27:02 +0100

    loaded habitable planets

--- nasa-project/server/src/server.js
+++ nasa-project/server/src/server.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
 const app = require("./app");
 const http = require("http");
+const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-	console.log(`listening on http://localhost:${PORT}`);
-});
+async function startServer() {
+	await loadPlanetsData();
+
+	server.listen(PORT, () => {
+		console.log(`listening on http://localhost:${PORT}`);
+	});
+}
+
+startServer();
